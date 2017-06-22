b2473f275110800d5b15b9caf9be94e13e39a63c
borisbokan <borisbokan@gmail.com>
2017-06-22 15:00:35 +0200

    Promene...koristim systemski content provider

---
+++ app/src/main/java/com/borcha/contentprovidervezba/Activitys/MainActivity.java
@@ -0,0 +1,45 @@
+package com.borcha.contentprovidervezba.Activitys;
+
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+
+import com.borcha.contentprovidervezba.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public
+    ContentResolver resolver;
+    Spinner spSlike;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        spSlike=(Spinner)findViewById(R.id.spinner);
+
+        resolver=getContentResolver();
+
+        Cursor cursorInternal=resolver.query( MediaStore.Images.Media.INTERNAL_CONTENT_URI,null,null,null,null);
+        Cursor cursorExternal=resolver.query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+
+        SimpleCursorAdapter curAdapter=new SimpleCursorAdapter(this,android.R.layout.simple_spinner_item,cursorExternal,new String[]{MediaStore.EXTRA_MEDIA_TITLE},new int[]{android.R.id.text1});
+
+        spSlike.setAdapter(curAdapter);
+
+
+/*     Custom
+       MyContentProvider myCont=new MyContentProvider();
+        myCont.query(BorchaContract.Podaci.URI_FOLDER,null,null,null,null);*/
+
+
+
+    }
+}
